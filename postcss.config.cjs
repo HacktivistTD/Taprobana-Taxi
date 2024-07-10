9128121c1d9c043697836488fa33296ab22d12b1
Thushara Dilruksha <124458363+HacktivistTD@users.noreply.github.com>
2024-07-10 22:01:52 +0530

    responsive Navbar

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
-=======
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
->>>>>>> 6a7fa965dbf1f979935ecaa09100e47789c73b9a
-  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,39 +1,36 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Header.css';
 import ESTD2009 from '../Images/ESTD2009.png';
+
 function Header() {
   return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" className="navigatinbarcont">  {/* Added collapseOnSelect and expand="lg" for responsiveness */}
-        <Container>
-        <img 
-             className="mx-auto h-14 w-auto rounded-full"
+    <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: '#f8e808', color: 'black' }}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img 
+            className="mx-auto h-14 w-auto rounded-full"
             src={ESTD2009}
             alt="TAPROBANA TAXI"
           />
-
-         
-           <Navbar.Toggle aria-controls="basic-navbar-nav" />  {/* Added toggle button for mobile responsiveness */}
-            <Nav className="text-center " id="basic-navbar-nav">
-            <Nav.Link href="/" className='navitems px-3'>   Home                   </Nav.Link>
-            <Nav.Link href="/contact" className='navitems px-3'> Contact Us        </Nav.Link>
-            <Nav.Link href="/about" className='navitems px-2'>  About              </Nav.Link>
-            <Nav.Link href="/tours" className='navitems px-3'>   Tours             </Nav.Link>
-            <Nav.Link href="/stay" className='navitems px-3'>   Stay Home             </Nav.Link>
-            <Nav.Link href="/fleet" className='navitems  flex lg:mr-96 px-3'> Choose Your Fleet   </Nav.Link>
-            <div className="loginalign flex lg:ml-96">
-           
-            <Nav.Link href="/login" className='navitems'>  Login              </Nav.Link>
-              
-                </div>
-           
-            {/* Removed duplicate "Stay Home" link */}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />  {/* Toggle button for mobile responsiveness */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto text-center">
+            <Nav.Link href="/" className='navitems px-3' style={{ color: 'black', fontWeight: 'bold' }}>Home</Nav.Link>
+            <Nav.Link href="/contact" className='navitems px-3' style={{ color: 'black', fontWeight: 'bold' }}>Contact Us</Nav.Link>
+            <Nav.Link href="/about" className='navitems px-2' style={{ color: 'black', fontWeight: 'bold' }}>About</Nav.Link>
+            <Nav.Link href="/tours" className='navitems px-3' style={{ color: 'black', fontWeight: 'bold' }}>Tours</Nav.Link>
+            <Nav.Link href="/stay" className='navitems px-3' style={{ color: 'black', fontWeight: 'bold' }}>Stay Home</Nav.Link>
+            <Nav.Link href="/fleet" className='navitems px-3' style={{ color: 'black', fontWeight: 'bold' }}>Choose Your Fleet</Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    </div>
+          <Nav>
+            <Nav.Link href="/login" className='navitems px-3' style={{ color: 'black', fontWeight: 'bold' }}>Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
