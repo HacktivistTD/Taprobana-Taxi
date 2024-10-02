ded51b1c278cc06f78e4fc854998b7188e3c6cb5
Thushara Dilruksha <124458363+HacktivistTD@users.noreply.github.com>
2024-10-02 23:38:49 +0530

    change eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,14 +11,23 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
+    // Warn if using target="_blank" without rel="noopener noreferrer"
+    'react/jsx-no-target-blank': ['warn', { allowReferrer: true }],
+    // Allow exporting constant values
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
